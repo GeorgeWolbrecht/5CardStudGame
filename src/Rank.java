9afb6ec4984ed4112fcb04fcabf28d1eec9207a6
Brian-SFA <womackbk@jacks.sfasu.edu>
2025-11-20 18:21:05 -0600

    Create Rank.java enumerator

---
+++ src/Rank.java
@@ -0,0 +1,8 @@
+public enum Rank {
+    TWO(2,"2"), THREE(3,"3"), FOUR(4,"4"), FIVE(5,"5"), SIX(6,"6"),
+    SEVEN(7,"7"), EIGHT(8,"8"), NINE(9,"9"), TEN(10,"10"),
+   JACK(11,"J"), QUEEN(12,"Q"), KING(13,"K"), ACE(14,"A");
+    final int value; final String symbol;
+    Rank(int v,String s){value=v;symbol=s;}
+
+}
